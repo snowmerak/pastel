ca470fe64a44dbbe11333856887328ab6593fa07
snowmerak <snowmerak@outlook.com>
2023-10-09 04:24:26 +0900

    Update example.dart

--- example/example.dart
+++ example/example.dart
@@ -1,46 +1,6 @@
-import 'dart:io';
-
 import 'package:pastel/pastel.dart';
 
-class FileNotExistsError extends PastelError {
-  final String path;
-
-  FileNotExistsError(this.path);
-
-  @override
-  String toString() => 'File $path does not exist';
-}
-
-class FileSystemError extends PastelError {
-  final FileSystemException exception;
-
-  FileSystemError(this.exception);
-
-  @override
-  String toString() => exception.toString();
-}
-
 void main() {
-  var result = Ok(File('pubspec.yaml')).map((file) {
-    if (file.existsSync()) {
-      return Ok(file);
-    } else {
-      return Err<File>(FileNotExistsError(file.path));
-    }
-  }).map((file) {
-    try {
-      return Ok(file.readAsStringSync());
-    } on FileSystemException catch (e) {
-      return Err<String>(FileSystemError(e));
-    }
-  });
-
-  switch (result) {
-    case Ok<String>():
-      print(result.unwrap());
-      break;
-    case Err<String>():
-      print(result.unwrapErr());
-      break;
-  }
+  final a = ImmutableList.from([1, 2, 3, 4, 5]);
+  final iter = a.toIterable();
 }
