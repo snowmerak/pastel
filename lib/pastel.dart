02c0ff5165f077faa90eda9478ed7d015ded1026
snowmerak <snowmerak@outlook.com>
2023-10-08 17:44:41 +0900

    make pastel error

--- lib/pastel.dart
+++ lib/pastel.dart
@@ -3,5 +3,6 @@
 /// More dartdocs go here.
 library;
 
+export 'src/monad/error.dart';
 export 'src/monad/result.dart';
 export 'src/monad/option.dart';
---
+++ lib/src/monad/error.dart
@@ -0,0 +1,19 @@
+sealed class PastelError {}
+
+class ExceptionError extends PastelError {
+  final Exception exception;
+
+  ExceptionError(this.exception);
+
+  @override
+  String toString() => exception.toString();
+}
+
+class StringError extends PastelError {
+  final String message;
+
+  StringError(this.message);
+
+  @override
+  String toString() => message;
+}
