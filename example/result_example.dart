71fa13996546eda7b4695156aa28d1cb8a2dbce2
snowmerak <snowmerak@outlook.com>
2023-10-08 21:28:37 +0900

    replace example

--- example/result_example.dart
+++ example/result_example.dart
@@ -1,10 +1,46 @@
+import 'dart:io';
+
 import 'package:pastel/pastel.dart';
 
+class FileNotExistsError extends PastelError {
+  final String path;
+
+  FileNotExistsError(this.path);
+
+  @override
+  String toString() => 'File $path does not exist';
+}
+
+class FileSystemError extends PastelError {
+  final FileSystemException exception;
+
+  FileSystemError(this.exception);
+
+  @override
+  String toString() => exception.toString();
+}
+
 void main() {
-  // Result
-  Result<int> result = Ok(1);
+  var result = Ok(File('pubspec.yaml')).map((file) {
+    if (file.existsSync()) {
+      return Ok(file);
+    } else {
+      return Err<File>(FileNotExistsError(file.path));
+    }
+  }).map((file) {
+    try {
+      return Ok(file.readAsStringSync());
+    } on FileSystemException catch (e) {
+      return Err<String>(FileSystemError(e));
+    }
+  });
 
-  print(result.isOk()); // true
-
-  print(result.map((p0) => Ok(p0 + 1))); // Ok(2)
+  switch (result) {
+    case Ok<String>():
+      print(result.unwrap());
+      break;
+    case Err<String>():
+      print(result.unwrapErr());
+      break;
+  }
 }
