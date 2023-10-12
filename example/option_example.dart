4fb6a2370de734fe84b6256da6a3387fd0946a23
snowmerak <snowmerak@outlook.com>
2023-10-12 20:03:15 +0900

    update return type changes

--- example/option_example.dart
+++ example/option_example.dart
@@ -4,10 +4,10 @@ void main() {
   Option<int> option = Some(1);
 
   print(option.isSome()); // true
-  print(option.map((p0) => p0 + 1)); // Some(2)
+  print(option.map((p0) => Some(p0 + 1))); // Some(2)
 
   option = None<int>();
 
   print(option.isNone()); // true
-  print(option.map((p0) => p0 + 1)); // None()
+  print(option.map((p0) => Some(p0 + 1))); // None()
 }
